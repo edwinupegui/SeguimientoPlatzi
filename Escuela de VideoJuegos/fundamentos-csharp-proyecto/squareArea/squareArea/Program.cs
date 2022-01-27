e15bf2aa454f0d264a1c1c8238e54604de2ff4e6
Edwin Upegui <edwinupegui@gmail.com>
2022-01-27 18:39:15 -0500

    clases del curso de C#

---
+++ Escuela de VideoJuegos/fundamentos-csharp-proyecto/squareArea/squareArea/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace squareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //rectangle area calc.
+
+            //rectangle values
+            float sideA = 10.0f;
+            int sideB = 1;
+            sideB++;
+            sideB++;
+            sideB++;
+            sideB--;
+            //rectangle area formula is a*b
+            float area = sideA * sideB;
+
+
+            Console.WriteLine("The rectangle area: " + area);
+
+        }
+    }
+}
