15b709da53b063eaa3b21e2b36d8bbeea50a0fc9
Edwin Upegui <edwinupegui@gmail.com>
2022-04-05 11:37:18 -0500

    progreso hasta la clase 3

---
+++ Escuela de JavaScript/Frontend con React.js/Curso de Unit Testing con Jest en React/jsbase/src/__test__/global.test.js
@@ -0,0 +1,10 @@
+const text = 'Hola Mundo';
+const fruits = ['Banana', 'Manzana', 'Melon', 'Uva'];
+
+test('Debe contener un texto', () => {
+  expect(text).toMatch(/Mundo/);
+}); 
+
+test('¿Tenemos un a banana?', () => {
+  expect()
+})
