9e3e107616bb2ba2594eff2ccb9caceb2f8b24ac
Edwin Upegui <edwinupegui@gmail.com>
2022-04-05 11:55:23 -0500

    progreso hasta la clase 4

--- Escuela de JavaScript/Frontend con React.js/Curso de Unit Testing con Jest en React/jsbase/src/__test__/global.test.js
+++ Escuela de JavaScript/Frontend con React.js/Curso de Unit Testing con Jest en React/jsbase/src/__test__/global.test.js
@@ -1,10 +1,34 @@
 const text = 'Hola Mundo';
 const fruits = ['Banana', 'Manzana', 'Melon', 'Uva'];
 
+
+// Comparación de textos
 test('Debe contener un texto', () => {
   expect(text).toMatch(/Mundo/);
 }); 
-
-test('¿Tenemos un a banana?', () => {
-  expect()
+// Comparación de elementos en un arreglo
+test('¿Tenemos una banana?', () => {
+  expect(fruits).toContain('Banana');
+});
+// Comparación de numeros (Mayor que)
+test('Mayor que', () => {
+  expect(10).toBeGreaterThan(9);
+});
+// Validación de booleanos función para verdaderos
+test('Verdadero', () => {
+  expect(true).toBeTruthy();
 })
+// Validación de booleanos función para falsos
+test('falso', () => {
+  expect(false).toBeFalsy();
+})
+
+const reverseString = (str, callback) => {
+  callback(str.split('').reverse().join(''));
+};
+
+test('probar callback', () => {
+  reverseString('Hola', (str) => {
+    expect(str).toBe('aloH');
+  });
+});
