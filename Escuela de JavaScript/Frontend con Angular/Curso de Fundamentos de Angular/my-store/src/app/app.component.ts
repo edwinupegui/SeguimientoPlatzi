6484c122873e983e244c785c56936d66469e8a83
Edwin Upegui <edwinupegui@gmail.com>
2022-05-24 22:39:45 -0500

    Progreso hasta la clase 17

--- Escuela de JavaScript/Frontend con Angular/Curso de Fundamentos de Angular/my-store/src/app/app.component.ts
+++ Escuela de JavaScript/Frontend con Angular/Curso de Fundamentos de Angular/my-store/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg = 10;
   name = 'Edwin';
   age = 50;
   img = 'https://josefacchin.com/wp-content/uploads/2020/02/como-quitar-el-fondo-de-una-imagen.png'
@@ -19,6 +22,40 @@ export class AppComponent {
   emojis: string[] = [ '😂' , '🐦', '🐳','🌮', '💚'];
 
   newEmoji = '';
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all'
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
 
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
