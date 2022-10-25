e03f391e7e1d339c306ac54722639f04324fb55a
Edwin Arley Upegui Vergara <edwinupegui@gmail.com>
2022-10-25 17:34:24 -0500

    avance

--- Escuela de JavaScript/Full Stack con Next.js/CursoDeNextJs/pages/product/[productId].tsx
+++ Escuela de JavaScript/Full Stack con Next.js/CursoDeNextJs/pages/product/[productId].tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from 'react';
+import { useRouter } from 'next/router';
 
 const ProductItem = () => {
-  var product = 'test'
+  var product = 'test';
   const {
     query : { productId },  
   } = useRouter();
