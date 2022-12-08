fe1aadd6f88ae5278e5a32d862e8cf1ce2d55537
Edwin Upegui <edwinupegui@gmail.com>
2022-12-08 17:33:26 -0500

    progreso hasta la clase 5

--- Escuela de JavaScript/JavaScript/Curso de Consumo de API REST con JavaScript/project/main.js
+++ Escuela de JavaScript/JavaScript/Curso de Consumo de API REST con JavaScript/project/main.js
@@ -1,10 +1,17 @@
-console.log('edwin');
+const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3';
 
-const URL = 'https://api.thedogapi.com/v1/images/search';
+const reload = async () => {
+  const res = await fetch(API_URL);
+  const data = await res.json();
 
-fetch(URL)
-  .then(res => res.json())
-  .then(data => {
-    const img = document.querySelector('img');
-    img.src = data[0].url;
-  })
+  console.log(data)
+  const img1 = document.getElementById('imagen1');
+  const img2 = document.getElementById('imagen2');
+  const img3 = document.getElementById('imagen3');
+  
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
+} 
+
+reload()
