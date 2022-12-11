b612aeacc1f5325e5dbc7d9b62d01b874250ceac
Edwin Upegui <edwinupegui@gmail.com>
2022-12-11 14:19:54 -0500

    avance

--- Escuela de JavaScript/JavaScript/Curso de Consumo de API REST con JavaScript/project/main.js
+++ Escuela de JavaScript/JavaScript/Curso de Consumo de API REST con JavaScript/project/main.js
@@ -1,17 +1,27 @@
-const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3';
+const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_r5nbzZaYNwp2fWHT31lZyGrms3uyeToAfjSJfzCHxknrmMpqfx8RkVxlULAZhycV';
+const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?limit=2&api_key=live_r5nbzZaYNwp2fWHT31lZyGrms3uyeToAfjSJfzCHxknrmMpqfx8RkVxlULAZhycV';
 
-const reload = async () => {
-  const res = await fetch(API_URL);
+const loadRandomDogs = async () => {
+  const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
 
   console.log(data)
   const img1 = document.getElementById('imagen1');
   const img2 = document.getElementById('imagen2');
-  const img3 = document.getElementById('imagen3');
   
   img1.src = data[0].url;
   img2.src = data[1].url;
-  img3.src = data[2].url;
+} 
+const favoritesDogs = async () => {
+  const res = await fetch(API_URL_FAVORITES);
+  const data = await res.json();
+
+  console.log(data)
+  const img1 = document.getElementById('imagen1');
+  const img2 = document.getElementById('imagen2');
+  
+  img1.src = data[0].url;
+  img2.src = data[1].url;
 } 
 
-reload()
+loadRandomDogs()
