2ef1525ea73cc46aa448c87f07bfe037de367429
Edwin Upegui <edwinupegui@gmail.com>
2023-01-27 20:33:52 -0500

    progreso hasta la clase 2

--- Escuela de JavaScript/JavaScript/Taller Práctico de JavaScript Matemáticas y Estadística Básica/math.js
+++ Escuela de JavaScript/JavaScript/Taller Práctico de JavaScript Matemáticas y Estadística Básica/math.js
@@ -1,1 +1,10 @@
-console.log('test 2')
+const ladoTriangulo1  = 12;
+const ladoTriangulo2  = 12;
+const baseTriangulo   = 8;
+const alturaTriangulo = Math.sqrt((ladoTriangulo1 * ladoTriangulo1) - ((baseTriangulo / 2) * (baseTriangulo / 2)));
+
+const perimetroTriangulo = `${ladoTriangulo1 + ladoTriangulo2 + baseTriangulo} cm`;
+const areaTriangulo = `${((baseTriangulo * alturaTriangulo) / 2).toFixed(2)} cm²`;
+
+console.log(perimetroTriangulo)
+console.log(areaTriangulo)
