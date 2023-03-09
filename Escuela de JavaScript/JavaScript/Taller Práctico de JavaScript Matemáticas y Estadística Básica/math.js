167907b72c232f9b2e66a2e16e6109d98b0ef007
Edwin Upegui <edwinupegui@gmail.com>
2023-03-09 00:14:55 -0500

    feat: curso matematicas

--- Escuela de JavaScript/JavaScript/Taller Práctico de JavaScript Matemáticas y Estadística Básica/math.js
+++ Escuela de JavaScript/JavaScript/Taller Práctico de JavaScript Matemáticas y Estadística Básica/math.js
@@ -1,10 +1,89 @@
-const ladoTriangulo1  = 12;
-const ladoTriangulo2  = 12;
-const baseTriangulo   = 8;
-const alturaTriangulo = Math.sqrt((ladoTriangulo1 * ladoTriangulo1) - ((baseTriangulo / 2) * (baseTriangulo / 2)));
+console.group('Cuadrado') 
 
-const perimetroTriangulo = `${ladoTriangulo1 + ladoTriangulo2 + baseTriangulo} cm`;
-const areaTriangulo = `${((baseTriangulo * alturaTriangulo) / 2).toFixed(2)} cm²`;
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
 
-console.log(perimetroTriangulo)
-console.log(areaTriangulo)
+console.log({
+  ladoCuadrado,
+  perimetroCuadrado,
+  areaCuadrado,
+});
+
+function calcularCuadrado(lado) {
+  return {
+    perimetro: lado * 4,
+    area: lado * lado,
+  };
+}
+
+console.groupEnd('Cuadrado') 
+
+console.group('Triangulo')
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTrianguloBase = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
+
+function calcularTriangulo(lado1, lado2, base, altura) {
+  return {
+    perimetro: lado1 + lado2 + base,
+    area: (base * altura) / 2,
+  };
+}
+
+
+function calcularAlturaTriangulo(lado1, base) {
+  if (lado1 == base) {
+    console.warn('Este no es un triángulo isosceles');
+  } else {
+    // h = raizcuadrada(lado1**2 - (b**2)/4)
+    return Math.sqrt( (lado1 ** 2) - ( (base ** 2) ) / 4 );
+  }
+}
+
+
+
+console.log({
+  ladoTriangulo1,
+  ladoTriangulo2,
+  ladoTrianguloBase,
+  alturaTriangulo,
+  perimetroTriangulo,
+  areaTriangulo,
+});
+
+console.groupEnd('Triangulo') 
+
+console.group('Circle')
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+  radioCirculo,
+  diametroCirculo,
+  PI,
+  circunferencia,
+  areaCirculo,
+});
+
+function calcularCirculo(radio) {
+  const diametro = radio * 2;
+  const radioAlCuadrado = Math.pow(radio, 2);
+  
+  return {
+    circunferencia: diametro * Math.PI,
+    area: radioAlCuadrado * Math.PI,
+  };
+}
+
+console.groupEnd('Circle')
