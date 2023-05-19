2527656c7b5d92669945ad7ca34291672b87b417
Edwin Upegui <edwinupegui@gmail.com>
2023-05-19 10:20:53 -0500

    Curso finalizado

--- Escuela de JavaScript/JavaScript/Taller Práctico de JavaScript ¡Crea tu Primer Videojuego!/javascript-videojuego/maps.js
+++ Escuela de JavaScript/JavaScript/Taller Práctico de JavaScript ¡Crea tu Primer Videojuego!/javascript-videojuego/maps.js
@@ -1,53 +1,54 @@
-/*
- * Reglas:
- * El final de cada nivel debe ser el inicio del siguiente
-*/
-
 const emojis = {
   '-': ' ',
   'O': '🚪',
   'X': '💣',
   'I': '🎁',
   'PLAYER': '💀',
-  'BOMB_COLLISION': '🔥',
-  'GAME_OVER': '👎',
+  'BOMB_COLLISION': '💥',
+  'GAME_OVER': '😈',
   'WIN': '🏆',
-};
+  'HEART': '❤️',
+  'PARTY_1': '🎊',
+  'PARTY_2': '🎉'};
+  
+const defaultName = ['TzilaC4tz1n','LaM14killer','ElGatijo','Bayonetta','JUAN',
+                   'DarkWarrior','~K1rit0~','Big Papi','T0xic4','Joker']
+const map=[];
 
-const maps = [];
-maps.push(`
-  IXXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  OXXXXXXXXX
+map.push(`
+IXXXXXXXXX
+-XXXXXXXXX
+-XXXXXXXXX
+-XXXXXXXXX
+-XXXXXXXXX
+-XXXXXXXXX
+-XXXXXXXXX
+-XXXXXXXXX
+-XXXXXXXXX
+OXXXXXXXXX`);
+
+map.push(`
+O--XXXXXXX
+X--XXXXXXX
+XX----XXXX
+X--XX-XXXX
+X-XXX--XXX
+X-XXXX-XXX
+XX--XX--XX
+XX--XXX-XX
+XXXX---IXX
+XXXXXXXXXX
 `);
-maps.push(`
-  O--XXXXXXX
-  X--XXXXXXX
-  XX----XXXX
-  X--XX-XXXX
-  X-XXX--XXX
-  X-XXXX-XXX
-  XX--XX--XX
-  XX--XXX-XX
-  XXXX---IXX
-  XXXXXXXXXX
-  `);
-maps.push(`
-  I-----XXXX
-  XXXXX-XXXX
-  XX----XXXX
-  XX-XXXXXXX
-  XX-----XXX
-  XXXXXX-XXX
-  XX-----XXX
-  XX-XXXXXXX
-  XX-----OXX
-  XXXXXXXXXX
+
+map.push(`
+I-----XXXX
+XXXXX-XXXX
+XX----XXXX
+XX-XXXXXXX
+XX-----XXX
+XXXXXX-XXX
+XX-----XXX
+XX-XXXXXXX
+XX-----OXX
+XXXXXXXXXX
 `);
