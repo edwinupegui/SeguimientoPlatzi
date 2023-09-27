1c385c1df6455184bbf5d451d33ef93ec4ae618c
Edwin Upegui <edwinupegui@gmail.com>
2023-09-26 20:26:00 -0500

    avance hasta la clase 16

--- Escuela de JavaScript/TypeScript/ts-app/src/app/users/user.model.ts
+++ Escuela de JavaScript/TypeScript/ts-app/src/app/users/user.model.ts
@@ -1,16 +1,12 @@
+import { BaseModel } from './../base.model';
+
 export enum ROLES {
   ADMIN = "admin",
   SELLER = "seller",
   CUSTOMER = "customer",
 }
 
-
-export type User = {
+export interface User extends BaseModel {
   username: string;
   role: ROLES;
-}
-
-const nicoUser: User = {
-  username: 'nicobytes',
-  role: ROLES.SELLER
 }
