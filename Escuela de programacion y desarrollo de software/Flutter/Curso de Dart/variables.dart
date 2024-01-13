8c6d116b4b49957b8f7ae4921cc7a736df98f64f
Edwin Upegui <edwinupegui@gmail.com>
2024-01-13 15:44:54 -0500

    tipos de datos

---
+++ Escuela de programacion y desarrollo de software/Flutter/Curso de Dart/variables.dart
@@ -0,0 +1,13 @@
+void main () {
+  print('Tipos de datos');
+  bool booleano = true;
+  int numeroEntero = 15;
+  double numeroDecimal = 7.5;
+  String cadenaTexto = 'Hello World!';
+  List<String> letras = ['a', 'b'];
+  print(booleano);
+  print(numeroEntero);
+  print(numeroDecimal);
+  print(cadenaTexto);
+  print(letras);
+}
