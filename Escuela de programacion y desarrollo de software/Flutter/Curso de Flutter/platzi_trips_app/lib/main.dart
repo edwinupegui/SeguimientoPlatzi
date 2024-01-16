6e98928dc73af2ecb90d42ab1de935b0c2ab84a3
Edwin Upegui <edwinupegui@gmail.com>
2024-01-15 20:17:50 -0500

    test

--- Escuela de programacion y desarrollo de software/Flutter/Curso de Flutter/platzi_trips_app/lib/main.dart
+++ Escuela de programacion y desarrollo de software/Flutter/Curso de Flutter/platzi_trips_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Demo test',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: true,
