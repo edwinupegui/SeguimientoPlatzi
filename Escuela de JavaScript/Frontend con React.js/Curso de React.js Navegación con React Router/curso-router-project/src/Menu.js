97922992ff983505a39c3fa9a864412c924da8e3
Edwin Upegui <edwinupegui@gmail.com>
2024-03-10 15:49:36 -0500

    avance

--- Escuela de JavaScript/Frontend con React.js/Curso de React.js Navegación con React Router/curso-router-project/src/Menu.js
+++ Escuela de JavaScript/Frontend con React.js/Curso de React.js Navegación con React Router/curso-router-project/src/Menu.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Menu = () => {
   return (
-    <nav>Menu</nav>
+    <nav>
+      <ul>
+        {/* <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/blog'>Blog</Link>
+        </li>
+        <li>
+          <Link to='/profile'>Profile</Link>
+        </li> */}
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/blog'>Blog</NavLink>
+        </li>
+        <li>
+          <NavLink to='/profile'>Profile</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
