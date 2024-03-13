9df166d12d25a1831ccdf2016e4456f9e6ae9e0f
Edwin Upegui <edwinupegui@gmail.com>
2024-03-13 12:44:20 -0500

    avance hasta la clase 5

---
+++ Escuela de JavaScript/Frontend con React.js/Curso de State Machines en React/react-state-machine/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BaseLayout } from './Components/BaseLayout'
+
+function App() {
+  return (
+    <div className="App">
+      <BaseLayout />
+    </div>
+  );
+}
+
+export default App;
