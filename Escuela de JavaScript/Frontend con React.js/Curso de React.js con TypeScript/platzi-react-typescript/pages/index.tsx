d3605138df243b007913a650f1c2fd2f84522280
Edwin Upegui <edwinupegui@gmail.com>
2024-03-20 13:36:58 -0500

    avance

--- Escuela de JavaScript/Frontend con React.js/Curso de React.js con TypeScript/platzi-react-typescript/pages/index.tsx
+++ Escuela de JavaScript/Frontend con React.js/Curso de React.js con TypeScript/platzi-react-typescript/pages/index.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import type { MouseEventHandler } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { RandomFox } from './components/RandomFox'
+import { RandomFox } from "@/components/RandomFox";
+
+// generate simple unique id
+const generateId = (): string => {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+};
+
+// random number from 1 to 122
+const random = () => Math.floor(Math.random() * 122) + 1;
+
+type ImageItem = {
+  id: string;
+  url: string;
+};
+
 const Home: NextPage = () => {
-  const random = () => Math.floor(Math.random() * 123) + 1;
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+    const id = generateId();
+    const url = `https://randomfox.ca/images/${random()}.jpg`;
+    setImages([...images, { id, url }]);
+  };
 
   return (
     <div>
@@ -13,8 +38,19 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1 className="text-3xl font-bold underline">Hey Platzi 😎!</h1>
-        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`}/>
+        <div className="m-4">
+          <button
+            onClick={addNewFox}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          >
+            Add new fox
+          </button>
+        </div>
+        {images.map(({ id, url }) => (
+          <div className="p-4" key={id}>
+            <RandomFox image={url} />
+          </div>
+        ))}
       </main>
 
       <footer></footer>
