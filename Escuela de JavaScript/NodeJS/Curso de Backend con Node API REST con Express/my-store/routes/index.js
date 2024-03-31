0858b7ced5b5e9ca70c1ea3e82f734e6f07ec547
Edwin Upegui <edwinupegui@gmail.com>
2024-03-31 11:43:04 -0500

    avance hasta la clase 20

--- Escuela de JavaScript/NodeJS/Curso de Backend con Node API REST con Express/my-store/routes/index.js
+++ Escuela de JavaScript/NodeJS/Curso de Backend con Node API REST con Express/my-store/routes/index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 
-const productRouter = require('./products.route')
-const categoriesRouter = require('./categories.route')
-const usersRouter = require('./users.route')
+const productsRouter = require('./products.router');
+const categoriesRouter = require('./categories.router');
+const usersRouter = require('./users.router');
 
-const routerApi = (app) => {
-  const router = express.Router()
-
-  app.use('/api/v1', router)
-
-  router.use('/products', productRouter)
-  router.use('/categories', categoriesRouter)
-  router.use('/users', usersRouter)
+function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
 }
 
-module.exports = routerApi
+module.exports = routerApi;
--- Escuela de JavaScript/NodeJS/Curso de Backend con Node API REST con Express/my-store/services/product.service.js
+++ Escuela de JavaScript/NodeJS/Curso de Backend con Node API REST con Express/my-store/services/product.service.js
@@ -1,76 +1,76 @@
-const { faker } = require('@faker-js/faker')
+const faker = require('faker');
+const boom = require('@hapi/boom');
 
-class ProductService {
+class ProductsService {
 
-  constructor() {
+  constructor(){
     this.products = [];
-    this.faker();
+    this.generate();
   }
 
-  faker(quantity = 100) {
-    for (let i = 0; i < quantity; i++) {
+  generate() {
+    const limit = 100;
+    for (let index = 0; index < limit; index++) {
       this.products.push({
-        id: i + 1,
+        id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
-        image: faker.image.url()
+        image: faker.image.imageUrl(),
+        isBlock: faker.datatype.boolean(),
       });
     }
-
   }
 
-  create(product = {}) {
+  async create(data) {
     const newProduct = {
-      id: this.products[this.products.length - 1].id + 1,
-      name: product.name,
-      price: parseInt(product.price),
-      image: product.image
+      id: faker.datatype.uuid(),
+      ...data
     }
     this.products.push(newProduct);
-
     return newProduct;
   }
 
-  find(filter = {}) {
-    return new Promise((resolve, reject) => {
+  find() {
+    return new Promise((resolve) => {
       setTimeout(() => {
-        resolve(
-          this.products.slice(filter.offset | 0, filter.limit ? parseInt(filter.limit) + parseInt((filter.offset | 0)) : undefined)
-        )
-      }, 5000);
+        resolve(this.products);
+      }, 3000);
     })
   }
 
-
-  findOne(id) {
-    return this.products.find((p) => p.id = id);
-  }
-
-  update(id, changes = {}) {
-    const product = this.products.find((prod) => prod.id === id);
-    const idx = this.products.findIndex((prod) => prod.id === id);
-    if (product) {
-      this.products[idx] = {
-        id: product.id,
-        name: changes.name || product.name,
-        price: changes.price || product.price,
-        image: changes.image || product.image
-      };
-      return product;
-    } else {
-      throw new Error('product not found')
+  async findOne(id) {
+    const product = this.products.find(item => item.id === id);
+    if (!product) {
+      throw boom.notFound('product not found');
     }
-  }
-
-
-  delete(id) {
-    const product = { ...this.products.find((prod) => prod.id === id) };
-    console.log(product)
-    if (product) {
-      this.products = this.products.filter((p) => p.id !== id);
-      return product;
+    if (product.isBlock) {
+      throw boom.conflict('product is block');
     }
+    return product;
   }
+
+  async update(id, changes) {
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw boom.notFound('product not found');
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
+  }
+
+  async delete(id) {
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw boom.notFound('product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
+  }
+
 }
 
-module.exports = ProductService;
+module.exports = ProductsService;
