3148b39179ea94b1a68842f80d08da0d996a99a9
Edwin Upegui <edwinupegui@gmail.com>
2024-04-01 11:15:11 -0500

    avance hasta la clase 3

---
+++ Escuela de programacion y desarrollo de software/React Native/React Native - Manejo de Listas y Consumo de APIs/pokedex/src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: 'edwindev',
+  password: 'admin'
+}
+
+export const userDetails = {
+  username: 'edwindev',
+  firstName: 'Edwin',
+  lastName: 'Dev',
+  email: 'edwindev@gmail.com'
+}
