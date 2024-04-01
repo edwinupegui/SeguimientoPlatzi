363e59d49d592aabc12273b497e7aa08d4e1d73d
Edwin Upegui <edwinupegui@gmail.com>
2024-04-01 15:40:59 -0500

    avance

--- Escuela de programacion y desarrollo de software/React Native/React Native - Manejo de Listas y Consumo de APIs/pokedex/src/components/Auth/LoginForm.js
+++ Escuela de programacion y desarrollo de software/React Native/React Native - Manejo de Listas y Consumo de APIs/pokedex/src/components/Auth/LoginForm.js
@@ -1,7 +1,17 @@
 import { StyleSheet, View, Text, TextInput, Button, Keyboard } from 'react-native'
 import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export default function LoginForm() {
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validateOnChange: true,
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formValue) => { console.log(formValue) }
+  })
+
   return (
     <View>
       <Text style={styles.title}>Iniciar sesión</Text>
@@ -9,16 +19,33 @@ export default function LoginForm() {
         placeholder='Nombre de usuario'
         style={styles.input}
         autoCapitalize='none'
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue('username', text)}
       />
+      <Text style={styles.error}>{formik.errors.username}</Text>
       <TextInput
         placeholder='Contraseña'
         style={styles.input}
         autoCapitalize='none'
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue('password', text)}
       />
-      <Button title='Entrar' onPress={() => console.log('Entrando...')} />
+      <Text style={styles.error}>{formik.errors.password}</Text>
+      <Button style={styles.button} title='Entrar' onPress={formik.handleSubmit} />
+
     </View>
   )
+}
+
+const initialValues = () => {
+  return { username: '', password: '' }
+}
+const validationSchema = () => {
+  return {
+    username: Yup.string().required('El usuario es obligatorio'),
+    password: Yup.string().required('La contraseña es obligatoría')
+  }
 }
 
 const styles = StyleSheet.create({
@@ -35,5 +62,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10
+  },
+  error: {
+    textAlign: 'center',
+    color: "#f00",
+    marginTop: 5,
+  },
+  button: {
+    marginTop: 10
   }
 })
