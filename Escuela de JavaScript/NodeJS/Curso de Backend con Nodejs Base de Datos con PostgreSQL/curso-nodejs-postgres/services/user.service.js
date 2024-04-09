b8bb1dfd134aa52bddb67a3ba8497005511f9c7f
Edwin Upegui <edwinupegui@gmail.com>
2024-04-09 14:57:28 -0500

    avance hasta la clase 11

--- Escuela de JavaScript/NodeJS/Curso de Backend con Nodejs Base de Datos con PostgreSQL/curso-nodejs-postgres/services/user.service.js
+++ Escuela de JavaScript/NodeJS/Curso de Backend con Nodejs Base de Datos con PostgreSQL/curso-nodejs-postgres/services/user.service.js
@@ -3,10 +3,11 @@ const boom = require('@hapi/boom');
 const { models } = require('./../libs/sequelize');
 
 class UserService {
-  constructor() {}
+  constructor() { }
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data)
+    return newUser;
   }
 
   async find() {
@@ -15,17 +16,22 @@ class UserService {
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id)
+    if (!user) {
+      throw boom.notFound('user not found')
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id)
+    const response = await user.update(changes)
+    return response
   }
 
   async delete(id) {
+    const user = await this.findOne(id)
+    await user.destroy()
     return { id };
   }
 }
