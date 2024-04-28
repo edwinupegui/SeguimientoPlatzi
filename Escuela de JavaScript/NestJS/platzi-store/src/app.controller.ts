fb5b871d54e0494ae77d2b8d87e79bf8b28406e9
Edwin Upegui <edwinupegui@gmail.com>
2024-04-28 13:50:10 -0500

    avance hasta la clase 8

--- Escuela de JavaScript/NestJS/platzi-store/src/app.controller.ts
+++ Escuela de JavaScript/NestJS/platzi-store/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,5 +18,33 @@ export class AppController {
   @Get('/ruta/')
   hello() {
     return 'con /asa/s';
+  }
+
+  @Get('/products/filter')
+  getProductFilter() {
+    return `yo soy un filtro`;
+  }
+
+  @Get('/products/:id')
+  getProduct(@Param('id') id: string) {
+    return `product ${id}`;
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products limit ${limit} offset ${offset} brand ${brand}`;
+  }
+
+  @Get('/categories/:categoryId/products/:productId')
+  getCategory(
+    @Param('categoryId') categoryId: string,
+    @Param('productId') productId: string,
+  ) {
+    return `product ${productId}
+            category ${categoryId}`;
   }
 }
