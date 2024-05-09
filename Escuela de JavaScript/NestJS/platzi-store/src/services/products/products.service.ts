682e5d6257fb129fa48500f8129fc82d8a511537
Edwin Upegui <edwinupegui@gmail.com>
2024-05-08 21:36:27 -0500

    avance hasta la clase 14

---
+++ Escuela de JavaScript/NestJS/platzi-store/src/services/products/products.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from '../../entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private counterId = 1;
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'product 1',
+      description: 'description',
+      price: 30000,
+      stock: 30,
+      image: 'image',
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  create(payload: any) {
+    this.counterId = this.counterId + 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+}
