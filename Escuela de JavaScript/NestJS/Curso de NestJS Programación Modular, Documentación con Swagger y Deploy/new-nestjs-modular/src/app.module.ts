d1e580ac8bf9ee0873b99a270e9004128ce6e8a5
Edwin Upegui <edwinupegui@gmail.com>
2024-06-02 22:55:44 -0500

    avancehasta la clase 7

--- Escuela de JavaScript/NestJS/Curso de NestJS Programación Modular, Documentación con Swagger y Deploy/new-nestjs-modular/src/app.module.ts
+++ Escuela de JavaScript/NestJS/Curso de NestJS Programación Modular, Documentación con Swagger y Deploy/new-nestjs-modular/src/app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = 'gd9qwedqdhqw8ey';
+const API_KEY_PROD = 'dlknwednaswkdnx';
+
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'development' ? API_KEY : API_KEY_PROD,
+    },
+    {
+      provide: 'TASK',
+      useFactory: async (http: HttpService) => {
+        const request = http.get('https://jsonplaceholder.typicode.com/todos');
+        const task = await lastValueFrom(request);
+        return task.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
--- Escuela de JavaScript/NestJS/Curso de NestJS Programación Modular, Documentación con Swagger y Deploy/new-nestjs-modular/src/users/entities/order.entity.ts
+++ Escuela de JavaScript/NestJS/Curso de NestJS Programación Modular, Documentación con Swagger y Deploy/new-nestjs-modular/src/users/entities/order.entity.ts
@@ -1,1 +1,7 @@
-export class Order {}
+import { User } from './user.entity';
+import { Product } from './../../products/entities/product.entity';
+export class Order {
+  date: Date;
+  user: User;
+  products: Product[];
+}
