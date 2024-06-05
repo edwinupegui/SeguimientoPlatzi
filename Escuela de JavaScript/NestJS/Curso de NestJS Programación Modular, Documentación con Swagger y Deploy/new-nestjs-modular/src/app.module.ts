c3925dac19c2155cea6927db7c45ead9d19196e3
Edwin Upegui <edwinupegui@gmail.com>
2024-06-05 18:18:47 -0500

    avance hasta la clase 8

--- Escuela de JavaScript/NestJS/Curso de NestJS Programación Modular, Documentación con Swagger y Deploy/new-nestjs-modular/src/app.module.ts
+++ Escuela de JavaScript/NestJS/Curso de NestJS Programación Modular, Documentación con Swagger y Deploy/new-nestjs-modular/src/app.module.ts
@@ -5,19 +5,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-
-const API_KEY = 'gd9qwedqdhqw8ey';
-const API_KEY_PROD = 'dlknwednaswkdnx';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule],
+  imports: [UsersModule, ProductsModule, HttpModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'development' ? API_KEY : API_KEY_PROD,
-    },
     {
       provide: 'TASK',
       useFactory: async (http: HttpService) => {
